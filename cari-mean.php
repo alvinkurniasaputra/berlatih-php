95ea37958a425c1add71588b075f7f48c0b04f0e
alvinkurniasaputra <alvinkurn43@gmail.com>
2021-05-16 08:05:37 +0700

    First commit

---
+++ cari-mean.php
@@ -0,0 +1,20 @@
+<?php 
+
+function cariMean($arr)
+{
+    $sum = 0;
+    for ($i=0; $i < count($arr); $i++)
+    {
+        $sum += $arr[$i];
+    }
+    echo "Mean : ", (round($sum / count($arr))), "\n";
+    
+}
+
+echo cariMean([1,2,3,4,5]);
+echo cariMean([3,5,7,5,3]);
+echo cariMean([6,5,4,7,3]);
+echo cariMean([1,3,3]);
+echo cariMean([7,7,7,7,7]);
+
+?>
